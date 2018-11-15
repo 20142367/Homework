8faeab1de74e316bee9e91df6725cf0320925b30
무명 무명 <alsrl0033@gmail.com>
2018-11-15 21:48:06 +0900

    PP10.Polymorphism

--- GameFrameworkPP01_PP14/PP01.HelloSDL/Game.cpp
+++ GameFrameworkPP01_PP14/PP01.HelloSDL/Game.cpp
@@ -19,8 +19,17 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 			return false;
 		}
 
-		m_go.load(100, 100, 128, 82, "animate");
-		m_player.load(300, 300, 128, 82, "animate");
+		m_go = new GameObject();
+		m_player = new Player();
+		m_enemy = new Enemy();
+
+		m_go->load(100, 100, 128, 82, "animate");
+		m_player->load(300, 300, 128, 82, "animate");
+		m_enemy->load(0, 0, 128, 82, "animate");
+
+		m_gameObjects.push_back(m_go);
+		m_gameObjects.push_back(m_player);
+		m_gameObjects.push_back(m_enemy);
 
 		SDL_SetRenderDrawColor(m_pRenderer, 255, 0, 0, 255);
 	}
@@ -38,16 +47,20 @@ void Game::render()
 	// clear the renderer to the draw color
 	SDL_RenderClear(m_pRenderer);	// draw color로 render 지우기
 
-	m_go.draw(m_pRenderer);
-	m_player.draw(m_pRenderer);
+	for (std::vector <GameObject*> ::size_type i = 0; i != m_gameObjects.size(); i++)
+	{
+		m_gameObjects[i]->draw(m_pRenderer);
+	}
 
 	SDL_RenderPresent(m_pRenderer);	// 화면 제시
 }
 
 void Game::update() 
 {
-	m_go.update();
-	m_player.update();
+	for (std::vector <GameObject*> ::size_type i = 0; i != m_gameObjects.size(); i++)
+	{
+		m_gameObjects[i]->update();
+	}
 }
 
 void Game::clean() 
--- GameFrameworkPP01_PP14/PP01.HelloSDL/Game.h
+++ GameFrameworkPP01_PP14/PP01.HelloSDL/Game.h
@@ -3,6 +3,8 @@
 #include "TextureManager.h"
 #include "GameObject.h"
 #include "Player.h"
+#include "Enemy.h"
+#include <vector>
 
 class Game
 {
@@ -14,9 +16,11 @@ public:
 	void clean();
 	bool running() { return m_bRunning; }
 
+	std::vector<GameObject*> m_gameObjects;
 	
-	GameObject m_go;
-	Player m_player;
+	GameObject* m_go;
+	GameObject* m_player;
+	GameObject* m_enemy;
 
 private:
 	SDL_Window * m_pWindow;
